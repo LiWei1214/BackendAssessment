fb4b0e75d9a9d9410b02b001143b85811dd7f9f3
LiWei1214 <162666480+LiWei1214@users.noreply.github.com>
2024-08-07 13:56:56 +0800

    Add files via upload

---
+++ src/titleAggregator/titleAggregatorApplication.java
@@ -0,0 +1,11 @@
+package titleAggregator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class titleAggregatorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(titleAggregatorApplication.class, args);
+    }
+}
